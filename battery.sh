10738250e723f61aafb2734c44aa3f0bb8925f45
James Bailey <git@jdb.name>
2021-03-07 11:40:45 -0500

    Add mAh and State of charge and Current capacity.

--- battery.sh
+++ battery.sh
@@ -5,17 +5,17 @@ stats=`ioreg -f -r -c AppleSmartBattery \
 
 currcharge=`echo $stats \
 |sed -E -e 's/.*"AppleRawCurrentCapacity" = ([0-9]*).*/\1/g'`
-echo "Current charge "$currcharge
+echo "Current charge "$currcharge" mAh"
 
 maxcapacity=`echo $stats \
 |sed -E -e 's/.*"AppleRawMaxCapacity" = ([0-9]*).*/\1/g'`
-echo "Full charge capacity "$maxcapacity
+echo "Full charge capacity "$maxcapacity" mAh"
 
 echo "Charge at "`echo "scale=3;"$currcharge"/"$maxcapacity"*100" |bc -l`"% capacity"
 
 designcapacity=`echo $stats \
 |sed -E -e 's/.*"DesignCapacity" = ([0-9]*).*/\1/g'`
-echo "Design capacity "$designcapacity
+echo "Design capacity "$designcapacity" mAh"
 
 echo "Current full charge at "`echo "scale=3;"$maxcapacity"/"$designcapacity"*100" |bc -l`"% design capacity"
 
@@ -25,7 +25,7 @@ echo "Cycle count is "$cycles
 
 ktemp=`echo $stats \
 |sed -E -e 's/.*"Temperature" = ([0-9]+).*/\1/g'`
-echo "Battery temperature "`echo "scale=3;("$ktemp"/10-273.15)*(9/5)+32" |bc -l`
+echo "Battery temperature "`echo "scale=3;("$ktemp"/10-273.15)*(9/5)+32" |bc -l`" °F"
 
 watts=`echo $stats \
 |sed -E -e 's/.*"Watts"=([0-9]+).*/\1/'`
@@ -57,5 +57,14 @@ famcode=`echo $stats |sed -E -e 's/.*"FamilyCode"=([0-9]+).*/\1/' |bitwise -ww -
 if [ $famcode -ne 0 ]; then
 	echo "Power adapter connected"
 else
-	echo "Power adapter Disconnected"
+	echo "Power adapter disconnected"
 fi
+
+currentcapacity=`echo $stats \
+|sed -E -e 's/.*"CurrentCapacity" = ([0-9]*).*/\1/g'`
+echo "Current capacity "$currentcapacity"%"
+
+stateofcharge=`echo $stats \
+|sed -E -e 's/.*"StateOfCharge"=([0-9]*).*/\1/g'`
+echo "State of charge "$stateofcharge"%"
+
